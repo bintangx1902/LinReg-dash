67af9b39dddd9192418d268182fe5ec07368b7ff
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2025-12-01 10:25:10 +0700

    udah bisa save

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ class Result(models.Model):
     date = models.DateField(auto_now_add=True)
     harvest_power = models.FloatField(null=False, blank=False)
     result = models.FloatField(null=False, blank=False)
-    netto = models.FloatField(null=False, blank=False)
-    tonnage = models.FloatField(null=False, blank=False)
+    netto = models.FloatField(null=False, blank=False, default=0)
+    tonnage = models.FloatField(null=False, blank=False, default=0)
     harvest_result = models.FloatField(null=False, blank=False)
 
     def __str__(self):

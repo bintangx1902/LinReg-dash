fa9dbf67219a33fc6df6f0eb4fe4f98ae824b10b
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2025-12-01 11:30:59 +0700

    disable debug toolbar

--- LinReg/urls.py
+++ LinReg/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
-import debug_toolbar
+# import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,6 +32,6 @@ if not settings.DEBUG or settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-    urlpatterns = [
-                      path('__debug__/', include(debug_toolbar.urls)),
-                  ] + urlpatterns
+    # urlpatterns = [
+    #                   path('__debug__/', include(debug_toolbar.urls)),
+    #               ] + urlpatterns

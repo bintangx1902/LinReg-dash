9476068cec9dc3c2d2392207a02a8e745b26137c
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2025-12-01 11:18:09 +0700

    push updatet

--- LinReg/urls.py
+++ LinReg/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,8 +31,6 @@ urlpatterns = [
 if not settings.DEBUG or settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-    import debug_toolbar
 
     urlpatterns = [
                       path('__debug__/', include(debug_toolbar.urls)),
